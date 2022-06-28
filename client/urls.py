8184ef1802d27b12036a0c640e77a510f7b8aff6
dndus <onenavero@gmail.com>
2022-06-28 12:55:37 +0500

    commit1.5

--- client/urls.py
+++ client/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegistrationView
+from .views import RegistrationView, ClientLogin, client_logout
 
 app_name = 'client'
 
 urlpatterns = [
-    path('registration/', RegistrationView.as_view(), name='registration')
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('login/', ClientLogin.as_view(), name='login'),
+    path('logout/', client_logout, name='logout')
 ]
--- client/views.py
+++ client/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 class RegistrationView(View):
@@ -28,3 +30,40 @@ class RegistrationView(View):
         return render(request, 'layouts/form.html', {
             'form': form
         })
+
+
+class ClientLogin(View):
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+
+        request.title = _("Tizimga kirish")
+
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'layouts/form.html', {
+            'form': form
+        })
+
+    def post(self, request):
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+
+                messages.success(request, _("Xush kelibsiz"))
+                return redirect('main:index')
+
+            form.add_error('password', _("Login va/yoki parol noto'g'ri"))
+
+        return render(request, 'layouts/form.html', {
+            'form': form
+        })
+
+@login_required()
+def client_logout(request):
+    messages.success(request, _("Xayr"))
+    logout(request)
+
+    return redirect('main:index')
